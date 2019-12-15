a421c8bedaf694f0aec205b80aec47ae233e84c3
Franco Ponticelli <francop@google.com>
2019-12-15 16:07:20 -0700

    fix entries in index.ts

--- paperjs/src/index.ts
+++ paperjs/src/index.ts
@@ -1,6 +1,19 @@
+/*
+Copyright 2019 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 export * from './adapter'
 export * from './component'
-export * from './compund_path'
+export * from './compound_path'
 export * from './context'
 export * from './filter'
 export * from './for_each'
@@ -13,11 +26,14 @@ export * from './lazy'
 export * from './map'
 export * from './match'
 export * from './path'
+export * from './point_text'
 export * from './project'
 export * from './raster'
+export * from './shape'
+export * from './symbol_item'
 export * from './template'
 export * from './tempo_attributes'
-export * from './shape'
+export * from './tool'
 export * from './until'
 export * from './value'
 export * from './when'
