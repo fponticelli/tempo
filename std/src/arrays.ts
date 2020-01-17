3006423ee983e13af1b37b2c0c4deccd44e42cd9
Franco Ponticelli <franco.ponticelli@gmail.com>
2020-01-16 21:56:00 -0700

    added functions to arrays.ts

--- std/src/arrays.ts
+++ std/src/arrays.ts
@@ -12,8 +12,9 @@ limitations under the License.
 */
 
 import { Maybe, nothing } from './maybe'
+import { Ordering } from './ord'
 
-export const map = <A, B>(f: (a: A) => B, arr: A[]): B[] => {
+export const map = <A, B>(f: (a: A) => B, arr: ReadonlyArray<A>): ReadonlyArray<B> => {
   const length = arr.length
   const buff = new Array(length)
   for (let i = 0; i < length; i++) {
@@ -22,7 +23,7 @@ export const map = <A, B>(f: (a: A) => B, arr: A[]): B[] => {
   return buff
 }
 
-export const flatMap = <A, B>(f: (a: A) => B[], arr: A[]): B[] => {
+export const flatMap = <A, B>(f: (a: A) => ReadonlyArray<B>, arr: ReadonlyArray<A>): ReadonlyArray<B> => {
   const buff = new Array()
   for (const el of arr) {
     buff.push(...f(el))
@@ -30,5 +31,67 @@ export const flatMap = <A, B>(f: (a: A) => B[], arr: A[]): B[] => {
   return buff
 }
 
-export const head = <A>(arr: A[]): Maybe<A> => arr.length > 0 ? arr[0] : nothing
-export const tail = <A>(arr: A[]): A[] => arr.slice(1)
+export const head = <A>(arr: ReadonlyArray<A>): Maybe<A> => arr.length > 0 ? arr[0] : nothing
+export const tail = <A>(arr: ReadonlyArray<A>): ReadonlyArray<A> => arr.slice(1)
+
+export const equals = <T>(predicate: (a: T, b: T) => boolean, a: ReadonlyArray<T>, b: ReadonlyArray<T>): boolean => {
+  if (a.length !== b.length)
+    return false
+  else {
+    for (let i = 0; i < a.length; i++) {
+      if (!predicate(a[i], b[i])) return false
+    }
+    return true
+  }
+}
+
+export const isEmpty = <T>(arr: ReadonlyArray<T>): arr is [] => arr.length === 0
+export const hasValues = <T>(arr: ReadonlyArray<T>): arr is [T, ...T[]] => arr.length > 0
+
+export const filter = <T>(predicate: (v: T) => boolean, arr: ReadonlyArray<T>): ReadonlyArray<T> => {
+  const buff = [] as T[]
+  for (const a of arr)
+    if (predicate(a))
+      buff.push(a)
+  return buff
+}
+
+export const flatten = <T>(arr: ReadonlyArray<ReadonlyArray<T>>): ReadonlyArray<T> => {
+  return ([] as ReadonlyArray<T>).concat(...arr)
+}
+
+export const foldLeft = <T, B>(f: (acc: B, curr: T) => B, arr: ReadonlyArray<T>, b: B): B => {
+  for (const a of arr) {
+    b = f(b, a)
+  }
+  return b
+}
+
+export const all = <T>(predicate: (v: T) => boolean, arr: ReadonlyArray<T>): boolean => {
+  for (const a of arr) {
+    if (!predicate(a)) {
+      return false
+    }
+  }
+  return true
+}
+
+export const any = <T>(predicate: (v: T) => boolean, arr: ReadonlyArray<T>): boolean => {
+  for (const a of arr) {
+    if (predicate(a)) {
+      return true
+    }
+  }
+  return false
+}
+
+export const each = <T>(f: (v: T) => void, arr: ReadonlyArray<T>): void => {
+  for (const a of arr)
+    f(a)
+}
+
+export const concat = <A>(...arrs: ReadonlyArray<A>[]): ReadonlyArray<A> =>
+  ([] as ReadonlyArray<A>).concat(...arrs)
+
+export const sort = <A>(compare: (a: A, b: A) => Ordering, arr: ReadonlyArray<A>): ReadonlyArray<A> =>
+  arr.slice().sort(compare)
