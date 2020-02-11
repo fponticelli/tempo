e7d0a04b8f6b8626cf566f23823b40a49dc6e8d8
Franco Ponticelli <francop@google.com>
2020-02-10 18:55:52 -0700

    [std] add JSON serialize/deserialize

--- std/src/json.ts
+++ std/src/json.ts
@@ -1,4 +1,41 @@
+/*
+Copyright 2019 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { Result, success, failure } from './result'
+
 export type JSONPrimitive = string | boolean | number | null
 export interface JSONObject extends Record<string, JSONValue> {}
 export type JSONArray = JSONValue[]
 export type JSONValue = JSONPrimitive | JSONObject | JSONArray
+
+export function deserialize(s: string): Result<JSONValue, string> {
+  try {
+    return success(JSON.parse(s))
+  } catch (e) {
+    return failure(e.message)
+  }
+}
+
+export function makeSerializer(options: { minified?: boolean, whitelist?: string[] }) {
+  return function serialize(v: JSONValue): string {
+    return JSON.stringify(v, options.whitelist, options.minified === false ? undefined : 2)
+  }
+}
+
+export function serialize(v: JSONValue): string {
+  return JSON.stringify(v, null, 2)
+}
+
+export function serializeMinified(v: JSONValue): string {
+  return JSON.stringify(v)
+}
