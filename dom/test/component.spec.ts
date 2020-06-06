698c80f9cabf75ba1e54dd94970c8317e19e34cd
Franco Ponticelli <franco.ponticelli@gmail.com>
2020-06-06 11:13:02 -0600

    update dependencies

--- dom/test/component.spec.ts
+++ dom/test/component.spec.ts
@@ -20,16 +20,13 @@ import { Property } from 'tempo-store/lib/property'
 
 describe('component', () => {
   it('fromElements sets-up the right defaults', () => {
-    const ctx = createContext()
+    const ctx = createContext<any>()
     const store = new Store(
       new Property(1),
       (state: number, action: number) => action
     )
 
-    const node = component(
-      { store },
-      div({}, text(String))
-    ).render(ctx, 2)
+    const node = component({ store }, div({}, text(String))).render(ctx, 2)
     expect(ctx.doc.body.innerHTML).toEqual('<div>2</div>')
     node.destroy()
     expect(ctx.doc.body.innerHTML).toEqual('')
